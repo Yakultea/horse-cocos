b2e0fdd96f2a2460aee05396cd5b21a59ff56480
Yakultea <maple99623@gmail.com>
2025-01-06 16:38:43 +0800

    更換achieve音效 & 1.0.40

--- proj/assets/scripts/game/model/GameConfigModel.ts
+++ proj/assets/scripts/game/model/GameConfigModel.ts
@@ -75,7 +75,7 @@ class GameConfigModel extends BaseModel<IGameConfigModel> {
             isRecordMode: false,
             framePerTime: 0.05,
             frameTimeRatio: 1,
-            version: 'v.1.0.39',
+            version: 'v.1.0.40',
             recordMode: ERecordMode.Default,
             renderMode: ERenderMode.Default,
         };
